8097838c1e3848770feb2dca521a320fd7c8dedc
Herbert <herbert.cardozo@gmail.com>
2024-07-30 19:17:21 -0300

    add login form

---
+++ src/components/forms/login/index.jsx
@@ -0,0 +1,15 @@
+export function LoginForm() {
+    return (
+        <>
+            <div className="form-control">
+                <form>
+                    <input type="email" placeholder="email" />
+                    <input type="password" placeholder="senha" />
+                    <button> Log in </button>
+
+                </form>
+            </div>
+
+        </>
+    )
+}
