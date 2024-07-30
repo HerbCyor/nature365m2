31bc0b4f5eec0f2df84cdd61d43c2aadb469b014
Herbert <herbert.cardozo@gmail.com>
2024-07-30 19:18:08 -0300

    add signup form

---
+++ src/components/forms/signup/index.jsx
@@ -0,0 +1,31 @@
+export function SignUpForm() {
+    return (
+        <>
+            <div className="form-control">
+                <form>
+                    <input type="text" placeholder="Nome Completo" />
+                    <input type="text" placeholder="Sexo" />
+                    <input type="text" placeholder="Data de Nascimento" />
+                    <input type="text" placeholder="CPF" />
+                    <input type="email" placeholder="email" />
+                    <input type="password" placeholder="senha" />
+
+                    <input type="text" placeholder="Rua" />
+                    <input type="text" placeholder="Número" />
+                    <input type="text" placeholder="Complemento" />
+                    <input type="text" placeholder="Bairro" />
+                    <input type="text" placeholder="Cidade" />
+                    <input type="text" placeholder="Estado" />
+                    <input type="text" placeholder="País" />
+                    <input type="text" placeholder="CEP" />
+
+
+
+                    <button> Log in </button>
+
+                </form>
+            </div>
+
+        </>
+    )
+}
